173e7498f0a2a4adb1c4559d83a268130d8ba8cb
cpea2506 <vienpro2506@gmail.com>
2022-01-07 03:40:52 +0700

    remove yarn global path

--- exports.zsh
+++ exports.zsh
@@ -2,6 +2,5 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 . "$HOME/.cargo/env"
 
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="$(yarn global bin):$PATH"
 export PATH="$HOME/.local/bin/heroku/bin/:$PATH"
 
